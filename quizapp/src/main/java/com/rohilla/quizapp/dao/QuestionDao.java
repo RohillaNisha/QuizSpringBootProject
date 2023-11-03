838d4a4bcaccffb8ebdadbc26e1b04074da53cbe
Nisha <nisha.rohilla@gmail.com>
2023-11-03 23:24:25 +0100

    fetching all questions from the database using jpa repository

---
+++ quizapp/src/main/java/com/rohilla/quizapp/dao/QuestionDao.java
@@ -0,0 +1,9 @@
+package com.rohilla.quizapp.dao;
+
+import com.rohilla.quizapp.Question;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface QuestionDao extends JpaRepository<Question, Integer> {
+}
---
+++ quizapp/src/main/java/com/rohilla/quizapp/service/QuestionService.java
@@ -0,0 +1,18 @@
+package com.rohilla.quizapp.service;
+
+import com.rohilla.quizapp.Question;
+import com.rohilla.quizapp.dao.QuestionDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class QuestionService {
+
+    @Autowired
+    QuestionDao questionDao;
+    public List<Question> getAllQuestions() {
+        return questionDao.findAll();
+        }
+}
